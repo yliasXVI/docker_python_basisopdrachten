dc8e59b26a9d389031825019cd9803d4dc0ac7e0
yliasXVI <121760433+yliasXVI@users.noreply.github.com>
2025-10-23 16:30:09 +0200

    040 opdr 1

--- app/040_lists/opdr_1/opdr_1.py
+++ app/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,16 @@
 # Opdracht 1 lists
-# Naam student:
-# Groep:
+# Naam student: Ylias Kok
+# Groep: 4ITX
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+gebruikers_gg = []
+gebruiker_1 = { "id": 0, "voornaam": "Bert", "achternaam": "Stoeptegel" }
+gebruiker_2 = { "id": 1, "voornaam": "Jan", "achternaam": "Frituurpan" }
+gebruiker_3 = { "id": 2, "voornaam": "Kees", "achternaam": "Weggeweest" }
+gebruiker_4 = { "id": 3, "voornaam": "Klaas", "achternaam": "Vaak" }
+
+gebruikers_gg.append(gebruiker_1)
+gebruikers_gg.append(gebruiker_2)
+gebruikers_gg.append(gebruiker_3)
+gebruikers_gg.append(gebruiker_4)
+
+print(gebruikers_gg[1]["voornaam"], gebruikers_gg[1]["achternaam"])
