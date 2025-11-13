ea1c7d18a1f2d220cd55af12f68118c037088366
yliasXVI <121760433+yliasXVI@users.noreply.github.com>
2025-11-13 15:54:34 +0100

    050 opdr 3 afgemaakt.

--- app/050_input_function/opdr_3/opdr_3.py
+++ app/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,7 @@
 
 # Hier komt je code...
 
+new_list = input("Vul minimaal 5 steden in, gescheiden door komma's: ")
+new_list = [item.strip() for item in new_list.split(",")]
+new_list.sort(reverse=True)
+print (new_list)
