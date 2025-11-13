7c6ea982820ec3d263f098db7b837ce269197868
yliasXVI <121760433+yliasXVI@users.noreply.github.com>
2025-11-13 14:38:16 +0100

    math works now

--- app/050_input_function/opdr_1/opdr_1.py
+++ app/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,13 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+import math
 
+a = int(input("Lengte van de eerste zijde \n"))
+b = int(input("Lengte van de tweede zijde \n"))
+
+x = ((a) **2)
+y = ((b) **2)
+z = (math.sqrt(x + y))
+
+print(z)
