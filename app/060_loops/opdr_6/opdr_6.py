012d1f50e74505c699f282e054c4c71e6dc9e006
yliasXVI <yliaskok@gmail.com>
2026-03-25 21:51:39 +0100

    opdr 6 060 done

--- app/060_loops/opdr_6/opdr_6.py
+++ app/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,37 @@
 
 # Hier start de for-loop
 
-my_list = []
+# Begin List
+my_list = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+# A --> Z List
+my_list.sort()
+print(my_list)
+
+# Add Item To List
+my_list.append('Hawaii')
+print(my_list)
+
+# Remove Item From List
+my_list.remove('verdi')
+print(my_list)
+
+# Show Only First 3 Items On List
+print(my_list [:3])
+
+# Show Only Items 1, 2, 3 On List
+print(my_list [1:4])
+
+# Show Only Last 3 Items On List
+print(my_list [2:])
+
+
+
+
+
+
+
+
+
+
+
