d09d2ffd241657ad6a045f630eba9082df2140e6
yliasXVI <yliaskok@gmail.com>
2026-04-16 10:03:36 +0200

    opdr 1 080

--- app/080_tekstbestanden/opdr_1/opdr_1.py
+++ app/080_tekstbestanden/opdr_1/opdr_1.py
@@ -3,3 +3,49 @@
 # Groep:
 
 # Jouw code komt hier
+lst = []
+
+Question1 = ('Wat vind je van de huidige regering?')
+print(Question1)
+lst.append(Question1)
+answer = input ()
+
+if answer == "":
+    print ('Geen anwoord ingevuld')
+    lst.append(answer)
+else: 
+    print (f"Bedankt voor jouw mening: {answer}")
+    lst.append(answer)
+
+Question2 = ('Wat vind je van de Python-lessen tot nu toe?')
+print(Question2)
+lst.append(Question2)
+answer = input ()
+
+if answer == "":
+    print ('Geen anwoord ingevuld')
+    lst.append(answer)
+else: 
+    print (f"Bedankt voor jouw mening: {answer}")
+    lst.append(answer)
+
+Question3 = ('Wat vind jij de mooiste stad van Nederland?')
+print(Question3)
+lst.append(Question3)
+answer = input ()
+
+if answer == "":
+    print ('Geen anwoord ingevuld')
+    lst.append(answer)
+else: 
+    print (f"Bedankt voor jouw mening: {answer}")
+    lst.append(answer)
+
+        
+fo = open('newtext.txt', 'wt')
+for answer in lst:
+    fo.write(answer + "\n")
+
+fo.close()
+
+
