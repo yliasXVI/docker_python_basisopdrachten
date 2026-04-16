0ae26dc3258936bbb738c294a72b984814853cc8
yliasXVI <yliaskok@gmail.com>
2026-04-16 10:56:25 +0200

    0dr 2 080

--- app/080_tekstbestanden/opdr_2/opdr_2.py
+++ app/080_tekstbestanden/opdr_2/opdr_2.py
@@ -7,4 +7,21 @@ prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
 
 # De rest moet jij doen.....
+pogingen = 0
+geraden = False
+
+while not geraden:
+    gok = int(input(prompt))
+    pogingen += 1
+
+    if gok < geheim_getal:
+        print ("hoger")
+    elif gok > geheim_getal:
+        print ("lager")
+    else:
+        geraden = True
+        print(f"Je hebt het getal geraden het is {geheim_getal}!")
+        print(f"Je hebt het in {pogingen} keer gedaan")
+
+
 
